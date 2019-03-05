559be7ba29cb57aa33701ebda3e87c617decad37
stefano <s.misuraca@globalscanning.com>
2019-03-05 11:12:59 +0100

    update project requirments

--- scaper_project/settings.py
+++ scaper_project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_mysql',
     'scraper_app',
     'django_extensions',
     'corsheaders',
